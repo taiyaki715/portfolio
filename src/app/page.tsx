ae0824ca52f5f96a438c37be5b6b556cd6bd8f46
taiki-ishikawa <taiyaki715@icloud.com>
2024-12-14 20:57:21 +0900

    refactor: skillデータを別ファイルに分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Divider } from "@nextui-org/divider";
-import { SkillChip } from "./components/skills/skillChip";
-import { SkillCard } from "./components/skills/skillCard";
+import { SkillChip } from "@/app/components/skills/skillChip";
+import { SkillCard } from "@/app/components/skills/skillCard";
+import { skillGenres } from "@/data/skills";
 
 export default function Home() {
   return (
@@ -18,39 +19,13 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <div className="font-black text-3xl">Skills</div>
         <div className="grid grid-cols-2 gap-4">
-          <SkillCard title="Languages">
-            <SkillChip label="TypeScript" />
-            <SkillChip label="PHP" />
-            <SkillChip label="Python" />
-            <SkillChip label="HTML" />
-            <SkillChip label="CSS" />
-          </SkillCard>
-          <SkillCard title="Frameworks & Libraries">
-            <SkillChip label="Next.js" />
-            <SkillChip label="Remix" />
-            <SkillChip label="Laravel" />
-            <SkillChip label="Tailwind CSS" />
-            <SkillChip label="Docker" />
-            <SkillChip label="Node.js" />
-            <SkillChip label="NPM" />
-            <SkillChip label="Biome" />
-          </SkillCard>
-          <SkillCard title="Databases & ORMs">
-            <SkillChip label="PostgreSQL" />
-            <SkillChip label="MySQL" />
-            <SkillChip label="Drizzle" />
-            <SkillChip label="Prisma" />
-          </SkillCard>
-          <SkillCard title="Cloud Services">
-            <SkillChip label="AWS" />
-            <SkillChip label="CloudFlare" />
-          </SkillCard>
-          <SkillCard title="Certificates">
-            <SkillChip label="応用情報技術者" />
-            <SkillChip label="ITパスポート" />
-            <SkillChip label="TOEIC 760点" />
-            <SkillChip label="アマチュア無線通信士第4級" />
-          </SkillCard>
+          { skillGenres.map((skillGenre) => (
+            <SkillCard title={skillGenre.title} key={skillGenre.title}>
+              { skillGenre.skills.map((skill) => (
+                <SkillChip label={skill} key={skill} />
+              ))}
+            </SkillCard>
+          ))}
         </div>
       </div>
       <Divider className="my-12" />
---
+++ src/data/skills/index.ts
@@ -0,0 +1,24 @@
+import type { SkillGenres } from "@/data/skills/skills.d";
+
+export const skillGenres: SkillGenres = [
+    {
+      title: "Languages",
+      skills: ["TypeScript", "PHP", "Python", "HTML", "CSS"]
+    },
+    {
+      title: "Frameworks & Libraries",
+      skills: ["Next.js", "Remix", "Laravel", "Tailwind CSS", "Docker", "Node.js", "NPM", "Biome"]
+    },
+    {
+      title: "Databases & ORMs",
+      skills: ["PostgreSQL", "MySQL", "Drizzle", "Prisma"]
+    },
+    {
+      title: "Cloud Services",
+      skills: ["AWS", "CloudFlare"]
+    },
+    {
+      title: "Certificates",
+      skills: ["応用情報技術者", "ITパスポート", "TOEIC 760点", "アマチュア無線通信士第4級"]
+    }
+];
