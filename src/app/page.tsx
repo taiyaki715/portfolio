93a8d4ed8f9f26b8409c995062a1d370dd1ff4df
taiki-ishikawa <taiyaki715@icloud.com>
2024-12-14 15:27:04 +0900

    feat: トップページにプロフィール画像等追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <div>
-        main
+      <div className="flex flex-col items-center gap-8">
+        <img src="/usi.jpg" alt="" className="w-[256px] rounded-full border" />
+        <div className="flex flex-col items-center gap-4">
+          <div className="font-black text-7xl">Hi, I'm Taiki Ishikawa.</div>
+          <div>A Software Engineer currently living in Japan.</div>
+        </div>
       </div>
     </div>
   );
