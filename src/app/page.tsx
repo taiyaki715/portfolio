ea16fe006cdb1ccf19b5ee22dcd651a3a6c15ba9
taiki-ishikawa <taiyaki715@icloud.com>
2024-12-14 20:31:17 +0900

    reafactor: SkillCardをコンポーネント化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Divider } from "@nextui-org/divider";
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import { SkillChip } from "./components/skills/skillChip";
+import { SkillCard } from "./components/skills/skillCard";
 
 export default function Home() {
   return (
@@ -18,54 +19,39 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <div className="font-black text-3xl">Skills</div>
         <div className="grid grid-cols-2 gap-4">
-          <Card className="flex-1">
-            <CardHeader className="font-bold">Languages</CardHeader>
-            <CardBody className="flex flex-row flex-wrap gap-2">
-              <SkillChip label="TypeScript" />
-              <SkillChip label="PHP" />
-              <SkillChip label="Python" />
-              <SkillChip label="HTML" />
-              <SkillChip label="CSS" />
-            </CardBody>
-          </Card>
-          <Card className="flex-1">
-            <CardHeader className="font-bold">Frameworks & Libraries</CardHeader>
-            <CardBody className="flex flex-row flex-wrap gap-2">
-              <SkillChip label="Next.js" />
-              <SkillChip label="Remix" />
-              <SkillChip label="Laravel" />
-              <SkillChip label="Tailwind CSS" />
-              <SkillChip label="Docker" />
-              <SkillChip label="Node.js" />
-              <SkillChip label="NPM" />
-              <SkillChip label="Biome" />
-            </CardBody>
-          </Card>
-          <Card className="flex-1">
-            <CardHeader className="font-bold">Databases & ORMs</CardHeader>
-            <CardBody className="flex flex-row flex-wrap gap-2">
-              <SkillChip label="PostgreSQL" />
-              <SkillChip label="MySQL" />
-              <SkillChip label="Drizzle" />
-              <SkillChip label="Prisma" />
-            </CardBody>
-          </Card>
-          <Card className="flex-1">
-            <CardHeader className="font-bold">Cloud Services</CardHeader>
-            <CardBody className="flex flex-row flex-wrap gap-2">
-              <SkillChip label="AWS" />
-              <SkillChip label="CloudFlare" />
-            </CardBody>
-          </Card>
-          <Card className="flex-1">
-            <CardHeader className="font-bold">Certificates</CardHeader>
-            <CardBody className="flex flex-row flex-wrap gap-2">
-              <SkillChip label="応用情報技術者" />
-              <SkillChip label="ITパスポート" />
-              <SkillChip label="TOEIC 760点" />
-              <SkillChip label="アマチュア無線通信士第4級" />
-            </CardBody>
-          </Card>
+          <SkillCard title="Languages">
+            <SkillChip label="TypeScript" />
+            <SkillChip label="PHP" />
+            <SkillChip label="Python" />
+            <SkillChip label="HTML" />
+            <SkillChip label="CSS" />
+          </SkillCard>
+          <SkillCard title="Frameworks & Libraries">
+            <SkillChip label="Next.js" />
+            <SkillChip label="Remix" />
+            <SkillChip label="Laravel" />
+            <SkillChip label="Tailwind CSS" />
+            <SkillChip label="Docker" />
+            <SkillChip label="Node.js" />
+            <SkillChip label="NPM" />
+            <SkillChip label="Biome" />
+          </SkillCard>
+          <SkillCard title="Databases & ORMs">
+            <SkillChip label="PostgreSQL" />
+            <SkillChip label="MySQL" />
+            <SkillChip label="Drizzle" />
+            <SkillChip label="Prisma" />
+          </SkillCard>
+          <SkillCard title="Cloud Services">
+            <SkillChip label="AWS" />
+            <SkillChip label="CloudFlare" />
+          </SkillCard>
+          <SkillCard title="Certificates">
+            <SkillChip label="応用情報技術者" />
+            <SkillChip label="ITパスポート" />
+            <SkillChip label="TOEIC 760点" />
+            <SkillChip label="アマチュア無線通信士第4級" />
+          </SkillCard>
         </div>
       </div>
       <Divider className="my-12" />
