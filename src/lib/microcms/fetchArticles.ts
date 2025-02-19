b714af5f87308431229f3fa2026fb80000f90a13
Taiki Ishikawa <124111090+taiyaki715@users.noreply.github.com>
2025-02-19 05:58:08 +0000

    shadcnインストール

--- src/lib/microcms/fetchArticles.ts
+++ src/lib/microcms/fetchArticles.ts
@@ -1,6 +1,6 @@
 import type { MicroCMSQueries } from "microcms-js-sdk";
-import type { BlogsResponse } from "./blog";
-import { client } from "./client";
+import type { BlogsResponse } from "@/lib/microcms/blog";
+import { client } from "@/lib/microcms/client";
 
 export const fetchArticles = async (queries?: MicroCMSQueries) => {
     return await client.get<BlogsResponse>({
