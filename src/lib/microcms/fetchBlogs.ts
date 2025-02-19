ebf58c15c13eb0a4d4dbe8624b2545a4ddbc0a15
Taiki Ishikawa <124111090+taiyaki715@users.noreply.github.com>
2025-02-19 07:55:24 +0000

    ページネーション一部実装

--- src/lib/microcms/fetchBlogs.ts
+++ src/lib/microcms/fetchBlogs.ts
@@ -2,7 +2,7 @@ import type { MicroCMSQueries } from "microcms-js-sdk";
 import type { BlogsResponse } from "@/lib/microcms/blog";
 import { client } from "@/lib/microcms/client";
 
-export const fetchArticles = async (queries?: MicroCMSQueries) => {
+export const fetchBlogs = async (queries?: MicroCMSQueries) => {
     return await client.get<BlogsResponse>({
         endpoint: "posts",
         queries: queries,
