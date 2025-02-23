b40011934099857409f8f4926ac683b9c3a165f5
Taiki Ishikawa <124111090+taiyaki715@users.noreply.github.com>
2025-02-23 00:13:48 +0000

    sitemapプラグインを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   vite: {
@@ -12,9 +13,9 @@ export default defineConfig({
   integrations: [
     react(),
     partytown({
-      config: {
-        forward: ["dataLayer.push"],
+    config: {
+      forward: ["dataLayer.push"],
       },
-    }),
-  ],
+    }), 
+    sitemap()],
 });
